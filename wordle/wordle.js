c1fde09007ff548ba4953f4dbb5da11da58b4094
SahibThethi <sahibthethi16@gmail.com>
2023-02-18 22:29:49 -0700

    Adding all the new/updated/fixed code for the html, css and javascript of the game

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -9,12 +9,17 @@ async function fetchData() {
       const dataObj = {
         dictionary: data.dictionary,
         secretWord: data.dictionary[index].word,
+        hint: data.dictionary[index].hint,
       }
       return dataObj;
 }
 
 async function game() {
+    loseCondition = 0;
+    winCondition = 0;
+    const game = document.getElementById("game");
     const x = await fetchData();
+    const { hint } = x;
     const { secretWord } = x;
     const state = {
         secretnum: String(secretWord).toUpperCase(),
@@ -41,6 +46,32 @@ async function game() {
         container.appendChild(box)
         return box
     }
+    // async function resetGame() {
+    //     if(winCondition > 0){
+    //         congrats.classList.toggle("hidden");
+    //         drawGrid(game);
+    //     }
+    //     state.currentRow = 0;
+    //     const row = state.currentRow;
+    //     for (let i = 0; i < 4; i++){
+    //         for(let j = 0; j < 4; j++){
+    //             const box = document.getElementById(`box ${i}${j}`);
+    //             box.classList.remove("right");
+    //             box.classList.remove("partial");
+    //             box.classList.remove("empty");
+    //         }
+    //     }
+    //     state.grid = Array(4)
+    //     .fill()
+    //     .map(() => Array(4).fill(""));
+    //     state.currentRow = 0;
+    //     state.currentCol = 0;
+    //     updateGrid();
+    //     if(loseCondition > 0){
+    //         lose.classList.toggle("hidden");
+    //     }
+        
+    // }        
     function drawGrid(container){
         const grid = document.createElement("div");
         grid.className = "grid";
@@ -50,6 +81,12 @@ async function game() {
             }
         }
         container.appendChild(grid)
+    }
+    function removeGrid(container){
+        const grid = container.querySelector(".grid");
+        if(grid){
+            container.removeChild(grid);
+        }
     }
     function registerKeyboardEvents() {
         document.body.onkeydown = (e) => {
@@ -84,6 +121,68 @@ async function game() {
         const words = dictionary.map(entry => entry.word.toUpperCase());
         return words.includes(String(userWord).toUpperCase());
     }
+    function getNumOfOccurrencesInWord(word, letter) {
+        let result = 0;
+        for (let i = 0; i < word.length; i++) {
+          if (word[i] === letter) {
+            result++;
+          }
+        }
+        return result;
+    }
+    function getPositionOfOccurrence(word, letter, position) {
+        let result = 0;
+        for (let i = 0; i <= position; i++) {
+          if (word[i] === letter) {
+            result++;
+          }
+        }
+        return result;
+    }
+    // function revealWord(guess) {
+    //     const row = state.currentRow;
+      
+    //     for (let i = 0; i < 4; i++) {
+    //       const box = document.getElementById(`box ${row}${i}`);
+    //       const letter = box.textContent;
+    //       const numOfOccurrencesSecret = getNumOfOccurrencesInWord(
+    //         state.secretnum,
+    //         letter
+    //       );
+    //       const numOfOccurrencesGuess = getNumOfOccurrencesInWord(guess, letter);
+    //       const letterPosition = getPositionOfOccurrence(guess, letter, i);
+    //       console.log(numOfOccurrencesGuess);
+    //       console.log(numOfOccurrencesSecret);
+    //       console.log(letterPosition);
+    //       console.log(numOfOccurrencesSecret);
+    //     if (
+    //         numOfOccurrencesGuess > numOfOccurrencesSecret &&
+    //         letterPosition > numOfOccurrencesSecret
+    //     ) {
+    //         box.classList.add("empty");
+    //     } else {
+    //         if (letter.toUpperCase() === state.secretnum[i]) {
+    //             box.classList.add("right");
+    //         } else if (state.secretnum.includes(letter.toUpperCase)) {
+    //             box.classList.add("partial");
+    //         } else {
+    //             box.classList.add("empty");
+    //         }
+    //         }
+    //     }
+    //     const isWinner = state.secretnum === guess;
+    //     const isGameOver = state.currentRow === 3;
+      
+    //     setTimeout(() => {
+    //       if (isWinner) {
+    //         alert('Congratulations!');
+    //       } else if (isGameOver) {
+    //         document.getElementById("lose").innerHTML = "You missed the word <b>" + state.secretnum + "</b> and lost!";
+    //         lose.classList.toggle("hidden");
+    //         loseCondition += 1;
+    //       }
+    //     },);
+    //   }
     function revealWord(guess){
         const row = state.currentRow;
         for (let i = 0; i < 4; i++){
@@ -97,18 +196,38 @@ async function game() {
                 box.classList.add("empty");
             }
         }
-
-        const winner = state.secretnum === String(guess);
-        const over = state.currentRow === 5;
-
-        if(winner){
-            console.log(winner);
-            alert("Congrats");
-        }else if (over) {
-            alert(`Better luck next time! The word was ${state.secretnum}`);
+        const isWinner = state.secretnum === guess.toUpperCase();
+        const isGameOver = state.currentRow === 3;
+        if (isWinner) {
+            removeGrid(game);
+            document.getElementById("congrats").innerHTML = `<img src="https://res.cloudinary.com/mkf/image/upload/v1675467141/ENSF-381/labs/congrats_fkscna.gif" alt="Congratulations" />`;
+            document.getElementById("win").innerHTML = "You guessed the word <b>" + state.secretnum + "</b> correctly!";
+            win.classList.toggle("hidden");
+        }else if (isGameOver) {
+            document.getElementById("lose").innerHTML = "You missed the word <b>" + state.secretnum + "</b> and lost!";
+            lose.classList.toggle("hidden");
         }
     }
-    document
+    function registerButtonEvents(){
+        const body=document.querySelector("body");
+        document.getElementById("startover").addEventListener("click",()=>{
+            location.reload();
+        });
+        document.getElementById("theme").addEventListener("click",()=>{
+            body.classList.toggle("dark-mode");
+            
+        });
+        document.getElementById("hintBlock").innerHTML = "<i>Hint: </i>" + hint;
+        const hintClick = document.getElementById("hint")
+        hintClick.addEventListener("click", () => {
+            hintBlock.classList.toggle("hidden");
+        });
+        const instructClick = document.getElementById("instruct")
+        const right = document.getElementById("right")
+        instructClick.addEventListener("click", () => {
+            right.classList.toggle("hidden2");
+        });
+    }
     function isLetter(key){
         return key.length == 1 && key.match(/[a-z]/i);
     }
@@ -123,9 +242,9 @@ async function game() {
         state.currentCol++;
     }
     async function startup() {
-        const game = document.getElementById("game");
         drawGrid(game);
         registerKeyboardEvents();
+        registerButtonEvents();
     }
     startup();
 }
