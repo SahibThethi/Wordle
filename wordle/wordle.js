d81932aeef447b7c7b868addcf640c1be5a05fbe
SahibThethi <sahibthethi16@gmail.com>
2023-02-19 18:30:17 -0700

    final edited and tested code

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -15,12 +15,10 @@ async function fetchData() {
 }
 
 async function game() {
-  loseCondition = 0;
-  winCondition = 0;
   const game = document.getElementById("game");
-  const x = await fetchData();
-  const { hint } = x;
-  const { secretWord } = x;
+  const fileData = await fetchData();
+  const { hint } = fileData;
+  const { secretWord } = fileData;
   const state = {
       secretnum: String(secretWord).toUpperCase(),
       grid: Array(4)
@@ -29,7 +27,6 @@ async function game() {
       currentRow: 0,
       currentCol: 0,
   };
-  console.log(state.secretnum);
   function updateGrid(){
       for(let i = 0; i < state.grid.length; i++){
           for(let j = 0; j < state.grid[i].length; j++){
@@ -45,33 +42,7 @@ async function game() {
       box.textContent = letter;
       container.appendChild(box)
       return box
-  }
-  // async function resetGame() {
-  //     if(winCondition > 0){
-  //         congrats.classList.toggle("hidden");
-  //         drawGrid(game);
-  //     }
-  //     state.currentRow = 0;
-  //     const row = state.currentRow;
-  //     for (let i = 0; i < 4; i++){
-  //         for(let j = 0; j < 4; j++){
-  //             const box = document.getElementById(`box ${i}${j}`);
-  //             box.classList.remove("right");
-  //             box.classList.remove("partial");
-  //             box.classList.remove("empty");
-  //         }
-  //     }
-  //     state.grid = Array(4)
-  //     .fill()
-  //     .map(() => Array(4).fill(""));
-  //     state.currentRow = 0;
-  //     state.currentCol = 0;
-  //     updateGrid();
-  //     if(loseCondition > 0){
-  //         lose.classList.toggle("hidden");
-  //     }
-      
-  // }        
+  }       
   function drawGrid(container){
       const grid = document.createElement("div");
       grid.className = "grid";
@@ -121,68 +92,6 @@ async function game() {
       const words = dictionary.map(entry => entry.word.toUpperCase());
       return words.includes(String(userWord).toUpperCase());
   }
-  function getNumOfOccurrencesInWord(word, letter) {
-      let result = 0;
-      for (let i = 0; i < word.length; i++) {
-        if (word[i] === letter) {
-          result++;
-        }
-      }
-      return result;
-  }
-  function getPositionOfOccurrence(word, letter, position) {
-      let result = 0;
-      for (let i = 0; i <= position; i++) {
-        if (word[i] === letter) {
-          result++;
-        }
-      }
-      return result;
-  }
-  // function revealWord(guess) {
-  //     const row = state.currentRow;
-    
-  //     for (let i = 0; i < 4; i++) {
-  //       const box = document.getElementById(`box ${row}${i}`);
-  //       const letter = box.textContent;
-  //       const numOfOccurrencesSecret = getNumOfOccurrencesInWord(
-  //         state.secretnum,
-  //         letter
-  //       );
-  //       const numOfOccurrencesGuess = getNumOfOccurrencesInWord(guess, letter);
-  //       const letterPosition = getPositionOfOccurrence(guess, letter, i);
-  //       console.log(numOfOccurrencesGuess);
-  //       console.log(numOfOccurrencesSecret);
-  //       console.log(letterPosition);
-  //       console.log(numOfOccurrencesSecret);
-  //     if (
-  //         numOfOccurrencesGuess > numOfOccurrencesSecret &&
-  //         letterPosition > numOfOccurrencesSecret
-  //     ) {
-  //         box.classList.add("empty");
-  //     } else {
-  //         if (letter.toUpperCase() === state.secretnum[i]) {
-  //             box.classList.add("right");
-  //         } else if (state.secretnum.includes(letter.toUpperCase)) {
-  //             box.classList.add("partial");
-  //         } else {
-  //             box.classList.add("empty");
-  //         }
-  //         }
-  //     }
-  //     const isWinner = state.secretnum === guess;
-  //     const isGameOver = state.currentRow === 3;
-    
-  //     setTimeout(() => {
-  //       if (isWinner) {
-  //         alert('Congratulations!');
-  //       } else if (isGameOver) {
-  //         document.getElementById("lose").innerHTML = "You missed the word <b>" + state.secretnum + "</b> and lost!";
-  //         lose.classList.toggle("hidden");
-  //         loseCondition += 1;
-  //       }
-  //     },);
-  //   }
   function revealWord(guess){
       const row = state.currentRow;
       for (let i = 0; i < 4; i++){
